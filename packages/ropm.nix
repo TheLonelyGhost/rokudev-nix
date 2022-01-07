52708afb965cfddbfb696dbe65efb870eff2545e
David Alexander <opensource@thelonelyghost.com>
2022-01-06 21:07:01 -0500

    Fixes unexpected dependency of ropm on npm

--- packages/ropm.nix
+++ packages/ropm.nix
@@ -4,8 +4,20 @@ let
   npm = import ../npm {
     inherit pkgs nodejs;
   };
+  deps = [
+    pkgs.nodePackages.npm
+  ];
 in
 npm.ropm.override {
+  buildInputs = [
+    pkgs.makeWrapper
+  ] ++ deps;
+
+  postInstall = ''
+    wrapProgram $out/bin/ropm --argv0 ropm \
+      --suffix PATH : ${pkgs.lib.makeBinPath deps}
+  '';
+
   meta = {
     description = "A package manager for the Roku platform";
     homepage = "https://github.com/rokucommunity/ropm";
